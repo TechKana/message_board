fc5fff914bf64f9de1a4392021720bb428b9baa9
kana-hon <kana-hon@NA21040.nature.local>
2021-07-20 11:43:10 +0900

    フラッシュメッセージを追加

--- src/controllers/DestroyServlet.java
+++ src/controllers/DestroyServlet.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import models.Message;
 import utils.DBUtil;
 
-
 @WebServlet("/destroy")
 public class DestroyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
@@ -21,19 +20,20 @@ public class DestroyServlet extends HttpServlet {
         super();
     }
 
-
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         String _token = request.getParameter("_token");
-        if(_token != null && _token.equals(request.getSession().getId())) {
+        if (_token != null && _token.equals(request.getSession().getId())) {
             EntityManager em = DBUtil.createEntityManager();
 
             // セッションスコープからメッセージのIDを取得して
             // 該当のIDのメッセージ1件のみをデータベースから取得
-            Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
+            Message m = em.find(Message.class, (Integer) (request.getSession().getAttribute("message_id")));
 
             em.getTransaction().begin();
-            em.remove(m);       // データ削除
+            em.remove(m); // データ削除
             em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "削除が完了しました。");
             em.close();
 
             // セッションスコープ上の不要になったデータを削除
